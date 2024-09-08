00901f42859ed0de797cc72f0ca9f39f61123003
muffin-lab <kirankannan.v@gmail.com>
2024-09-08 10:20:44 +0530

    updates

--- laraWeb/index.php
+++ laraWeb/index.php
@@ -6,8 +6,10 @@ require "functions.php";
 
 require 'Database.php';
 
-$db = new Database;
+$config = require 'config.php';
 
-$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config['database']);
+
+$posts = $db->query("SELECT * FROM posts")->fetchAll();
 
 dd($posts);
